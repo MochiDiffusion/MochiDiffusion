adeb1acde7f3ff0ef4126009e0b53c326139d476
godly-devotion <jpark@duck.com>
2023-02-11 22:10:49 -0500

    Revert "Inspector: Info text scrolls under image shadow"
    This reverts commit 03f9a7fecafb436197acc590154cf76411adf41e.

--- Mochi Diffusion/Views/InspectorView.swift
+++ Mochi Diffusion/Views/InspectorView.swift
@@ -53,78 +53,71 @@ struct InspectorView: View {
                 Image(img, scale: 1, label: Text(verbatim: String(sdi.prompt)))
                     .resizable()
                     .aspectRatio(contentMode: .fit)
+                    .padding(4)
                     .shadow(color: sdi.image?.averageColor ?? .black, radius: 16)
-                    .padding(.top)
-                    .padding(.top, 4) // Extra padding to match the border size of Gallery items
+                    .padding()
 
                 ScrollView(.vertical) {
                     Grid(alignment: .leading, horizontalSpacing: 4) {
-                        Spacer().frame(height: 16)
-                        Group {
-                            InfoGridRow(
-                                type: LocalizedStringKey(Metadata.date.rawValue),
-                                text: sdi.generatedDate.formatted(date: .long, time: .standard),
-                                showCopyToPromptOption: false
-                            )
-                            InfoGridRow(
-                                type: LocalizedStringKey(Metadata.model.rawValue),
-                                text: sdi.model,
-                                showCopyToPromptOption: false
-                            )
-                            InfoGridRow(
-                                type: LocalizedStringKey(Metadata.size.rawValue),
-                                text: "\(sdi.width) x \(sdi.height)\(!sdi.upscaler.isEmpty ? " (Upscaled using \(sdi.upscaler))" : "")",
-                                showCopyToPromptOption: false
-                            )
-                        }
-                        Group {
-                            InfoGridRow(
-                                type: LocalizedStringKey(Metadata.includeInImage.rawValue),
-                                text: sdi.prompt,
-                                showCopyToPromptOption: true,
-                                callback: store.copyPromptToPrompt
-                            )
-                            InfoGridRow(
-                                type: LocalizedStringKey(Metadata.excludeFromImage.rawValue),
-                                text: sdi.negativePrompt,
-                                showCopyToPromptOption: true,
-                                callback: store.copyNegativePromptToPrompt
-                            )
-                            InfoGridRow(
-                                type: LocalizedStringKey(Metadata.seed.rawValue),
-                                text: String(sdi.seed),
-                                showCopyToPromptOption: true,
-                                callback: store.copySeedToPrompt
-                            )
-                        }
-                        Group {
-                            InfoGridRow(
-                                type: LocalizedStringKey(Metadata.steps.rawValue),
-                                text: String(sdi.steps),
-                                showCopyToPromptOption: true,
-                                callback: store.copyStepsToPrompt
-                            )
-                            InfoGridRow(
-                                type: LocalizedStringKey(Metadata.guidanceScale.rawValue),
-                                text: String(sdi.guidanceScale),
-                                showCopyToPromptOption: true,
-                                callback: store.copyGuidanceScaleToPrompt
-                            )
-                            InfoGridRow(
-                                type: LocalizedStringKey(Metadata.scheduler.rawValue),
-                                text: sdi.scheduler.rawValue,
-                                showCopyToPromptOption: true,
-                                callback: store.copySchedulerToPrompt
-                            )
-                            InfoGridRow(
-                                type: LocalizedStringKey(Metadata.mlComputeUnit.rawValue),
-                                text: MLComputeUnits.toString(sdi.mlComputeUnit),
-                                showCopyToPromptOption: false
-                            )
-                        }
+                        InfoGridRow(
+                            type: LocalizedStringKey(Metadata.date.rawValue),
+                            text: sdi.generatedDate.formatted(date: .long, time: .standard),
+                            showCopyToPromptOption: false
+                        )
+                        InfoGridRow(
+                            type: LocalizedStringKey(Metadata.model.rawValue),
+                            text: sdi.model,
+                            showCopyToPromptOption: false
+                        )
+                        InfoGridRow(
+                            type: LocalizedStringKey(Metadata.size.rawValue),
+                            text: "\(sdi.width) x \(sdi.height)\(!sdi.upscaler.isEmpty ? " (Upscaled using \(sdi.upscaler))" : "")",
+                            showCopyToPromptOption: false
+                        )
+                        InfoGridRow(
+                            type: LocalizedStringKey(Metadata.includeInImage.rawValue),
+                            text: sdi.prompt,
+                            showCopyToPromptOption: true,
+                            callback: store.copyPromptToPrompt
+                        )
+                        InfoGridRow(
+                            type: LocalizedStringKey(Metadata.excludeFromImage.rawValue),
+                            text: sdi.negativePrompt,
+                            showCopyToPromptOption: true,
+                            callback: store.copyNegativePromptToPrompt
+                        )
+                        InfoGridRow(
+                            type: LocalizedStringKey(Metadata.seed.rawValue),
+                            text: String(sdi.seed),
+                            showCopyToPromptOption: true,
+                            callback: store.copySeedToPrompt
+                        )
+                        InfoGridRow(
+                            type: LocalizedStringKey(Metadata.steps.rawValue),
+                            text: String(sdi.steps),
+                            showCopyToPromptOption: true,
+                            callback: store.copyStepsToPrompt
+                        )
+                        InfoGridRow(
+                            type: LocalizedStringKey(Metadata.guidanceScale.rawValue),
+                            text: String(sdi.guidanceScale),
+                            showCopyToPromptOption: true,
+                            callback: store.copyGuidanceScaleToPrompt
+                        )
+                        InfoGridRow(
+                            type: LocalizedStringKey(Metadata.scheduler.rawValue),
+                            text: sdi.scheduler.rawValue,
+                            showCopyToPromptOption: true,
+                            callback: store.copySchedulerToPrompt
+                        )
+                        InfoGridRow(
+                            type: LocalizedStringKey(Metadata.mlComputeUnit.rawValue),
+                            text: MLComputeUnits.toString(sdi.mlComputeUnit),
+                            showCopyToPromptOption: false
+                        )
                     }
                 }
-                .padding(.horizontal)
+                .padding([.horizontal])
 
                 Divider()
 
