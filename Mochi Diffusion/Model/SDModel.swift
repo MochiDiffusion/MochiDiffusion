a499844e348faafe756ab1ed4cc8a33d48811ed5
ZachNagengast <znagengast@gmail.com>
2024-06-25 12:42:08 -0700

    Add sd3 pipeline to app

--- Mochi Diffusion/Model/SDModel.swift
+++ Mochi Diffusion/Model/SDModel.swift
@@ -17,6 +17,7 @@ struct SDModel: Identifiable {
     let attention: SDModelAttentionType
     let controlNet: [String]
     let isXL: Bool
+    let isSD3: Bool
     let inputSize: CGSize?
     let tokenizer: Tokenizer?
 
@@ -28,6 +29,7 @@ struct SDModel: Identifiable {
         }
 
         let isXL = identifyIfXL(url)
+        let isSD3 = identifyIfSD3(url)
         let size = identifyInputSize(url)
 
         self.url = url
@@ -40,6 +42,7 @@ struct SDModel: Identifiable {
             self.controlNet = []
         }
         self.isXL = isXL
+        self.isSD3 = isSD3
         self.inputSize = size
         self.tokenizer = Tokenizer(modelDir: url)
     }
@@ -108,11 +111,39 @@ private func identifyIfXL(_ url: URL) -> Bool {
     }
 }
 
+private func identifyIfSD3(_ url: URL) -> Bool {
+    guard let metadataURL = unetMetadataURL(from: url) else {
+        logger.warning("No model metadata found at '\(url)'")
+        return false
+    }
+
+    struct ModelMetadata: Decodable {
+        let inputSchema: [[String: String]]
+    }
+
+    do {
+        let jsonData = try Data(contentsOf: metadataURL)
+        let metadatas = try JSONDecoder().decode([ModelMetadata].self, from: jsonData)
+
+        guard metadatas.count == 1 else {
+            return false
+        }
+
+        // SD3 models have 4 inputs with one named "latent_image_embeddings"
+        let inputNames = metadatas[0].inputSchema.compactMap { $0["name"] }
+        return inputNames.contains("latent_image_embeddings")
+    } catch {
+        logger.warning("Failed to parse model metadata at '\(metadataURL)': \(error)")
+        return false
+    }
+}
+
 private func unetMetadataURL(from url: URL) -> URL? {
     let potentialMetadataURLs = [
         url.appending(components: "Unet.mlmodelc", "metadata.json"),
         url.appending(components: "UnetChunk1.mlmodelc", "metadata.json"),
         url.appending(components: "ControlledUnet.mlmodelc", "metadata.json"),
+        url.appending(components: "MultiModalDiffusionTransformer.mlmodelc", "metadata.json"),
     ]
 
     return potentialMetadataURLs.first {
