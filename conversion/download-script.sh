61e5e114af47df1f05d2a9bf6156471e895b28a9
Vladislav Ivanov <vlad@ivanov.email>
2024-12-22 13:32:51 +0100

    fix lockfile

---
+++ conversion/download-script.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+HF_DIFFUSERS_REVISION=76c645d3a641c879384afcb43496f0b7db8cc5cb
+
+wget https://raw.githubusercontent.com/huggingface/diffusers/${HF_DIFFUSERS_REVISION}/scripts/convert_original_stable_diffusion_to_diffusers.py
