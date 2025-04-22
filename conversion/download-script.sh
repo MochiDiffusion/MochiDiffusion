d6c9c8538def655957c36ec987c5a3a6fc87b154
Graham Bing <gdbing@users.noreply.github.com>
2025-04-22 11:09:10 -0700

    Switch from wget to curl in download-script.sh
    - Using curl for better compatibility and control over download options

--- conversion/download-script.sh
+++ conversion/download-script.sh
@@ -2,4 +2,5 @@
 
 HF_DIFFUSERS_REVISION=76c645d3a641c879384afcb43496f0b7db8cc5cb
 
-wget https://raw.githubusercontent.com/huggingface/diffusers/${HF_DIFFUSERS_REVISION}/scripts/convert_original_stable_diffusion_to_diffusers.py
+curl -L -o convert_original_stable_diffusion_to_diffusers.py \
+  https://raw.githubusercontent.com/huggingface/diffusers/${HF_DIFFUSERS_REVISION}/scripts/convert_original_stable_diffusion_to_diffusers.py
